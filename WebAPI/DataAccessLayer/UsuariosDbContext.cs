6a2f66812297be9722792947e1924becf4562e04
Fábio Dias <fabiodias.eng@gmail.com>
2020-11-08 00:52:08 -0300

    Client DbContext

---
+++ WebAPI/DataAccessLayer/UsuariosDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Eventos.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventos.WebAPI.DataAccessLayer
+{
+    public class UsuariosDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        public UsuariosDbContext(DbContextOptions<UsuariosDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
