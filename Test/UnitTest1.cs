e71726a7e3a900fe933239a17e8912e76b531868
Fábio Dias <fabiodias.eng@gmail.com>
2020-11-08 23:07:10 -0300

    faltando a parte de Testes

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace Eventos.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Padrão AAA
+
+
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+    }
+}
